bf3a58bd4503fc991c13669bb6210e41f3da174e
PauliusKuzmickas <pauliuskuzmickas@linux262.computing.dundee.ac.uk>
2016-10-25 18:37:41 +0100

    Did the logic for 1D.

---
+++ CA1DLogic.cpp
@@ -0,0 +1,138 @@
+/************************
+ 	James Strachan - 150008551
+	Paulius Kuzmickas - 150015860
+	AC21009 Assignment #2
+	Cellular Automaton
+*************************/
+#include <iostream>
+#include "CA1DLogic.h"
+
+using namespace std;
+
+
+	CA1DLogic::CA1DLogic(int width, int generations) {
+		WIDTH = width;
+		GENS = generations;
+	}
+
+	void CA1DLogic::printLine(int gen[]) {
+		for(int i=0; i<WIDTH; i++) {
+			if(gen[i]==0) cout << "\u2588";
+			else if(gen[i]==1) cout << "\u2591";
+		}
+		cout<<endl;
+	}
+
+	void CA1DLogic::printRule(int value) {
+		int ParentGen[WIDTH]; //the array holding the generation
+		setRule(value);
+		for(int i=0; i<WIDTH; i++) ParentGen[i] = 0;
+		ParentGen[WIDTH/2]=1;
+		printLine(ParentGen);
+		int currentLine[WIDTH];
+		for(int j=0; j<GENS; j++) {
+			for(int i=0; i<WIDTH; i++) {
+				if(i==0) {
+					if(ParentGen[i]==0 && ParentGen[i+1]==0 && ParentGen[WIDTH-1]==0) { // element 1
+						currentLine[i]=Rule[0];
+					}
+					else if(ParentGen[i]==0 && ParentGen[i+1]==1 && ParentGen[WIDTH-1]==0) { // element 1
+						currentLine[i]=Rule[1];
+					}
+					else if(ParentGen[i]==1 && ParentGen[i+1]==0 && ParentGen[WIDTH-1]==0) { // element 1
+						currentLine[i]=Rule[2];
+					}
+					else if(ParentGen[i]==1 && ParentGen[i+1]==1 && ParentGen[WIDTH-1]==0) { // element 1
+						currentLine[i]=Rule[3];
+					}
+					else if(ParentGen[i]==0 && ParentGen[i+1]==0 && ParentGen[WIDTH-1]==1) { // element 1
+						currentLine[i]=Rule[4];
+					}
+					else if(ParentGen[i]==0 && ParentGen[i+1]==1 && ParentGen[WIDTH-1]==1) { // element 1
+						currentLine[i]=Rule[5];
+					}
+					else if(ParentGen[i]==1 && ParentGen[i+1]==0 && ParentGen[WIDTH-1]==1) { // element 1
+						currentLine[i]=Rule[6];
+					}
+					else if(ParentGen[i]==1 && ParentGen[i+1]==1 && ParentGen[WIDTH-1]==1) { // element 1
+						currentLine[i]=Rule[7];
+					}
+				}
+				else if(i==WIDTH-1) {
+					if(ParentGen[i]==0 && ParentGen[0]==0 && ParentGen[i-1]==0) { // element 1
+						currentLine[i]=Rule[0];
+					}
+					else if(ParentGen[i]==0 && ParentGen[0]==1 && ParentGen[i-1]==0) { // element 1
+						currentLine[i]=Rule[1];
+					}
+					else if(ParentGen[i]==1 && ParentGen[0]==0 && ParentGen[i-1]==0) { // element 1
+						currentLine[i]=Rule[2];
+					}
+					else if(ParentGen[i]==1 && ParentGen[0]==1 && ParentGen[i-1]==0) { // element 1
+						currentLine[i]=Rule[3];
+					}
+					else if(ParentGen[i]==0 && ParentGen[0]==0 && ParentGen[i-1]==1) { // element 1
+						currentLine[i]=Rule[4];
+					}
+					else if(ParentGen[i]==0 && ParentGen[0]==1 && ParentGen[i-1]==1) { // element 1
+						currentLine[i]=Rule[5];
+					}
+					else if(ParentGen[i]==1 &&ParentGen[0]==0 && ParentGen[i-1]==1) { // element 1
+						currentLine[i]=Rule[6];
+					}
+					else if(ParentGen[i]==1 && ParentGen[0]==1 && ParentGen[i-1]==1) { // element 1
+						currentLine[i]=Rule[7];
+					}
+				}
+				else {
+					if(ParentGen[i]==0 && ParentGen[i+1]==0 && ParentGen[i-1]==0) { // element 1
+						currentLine[i]=Rule[0];
+					}
+					else if(ParentGen[i]==0 && ParentGen[i+1]==1 && ParentGen[i-1]==0) { // element 1
+						currentLine[i]=Rule[1];
+					}
+					else if(ParentGen[i]==1 && ParentGen[i+1]==0 && ParentGen[i-1]==0) { // element 1
+						currentLine[i]=Rule[2];
+					}
+					else if(ParentGen[i]==1 && ParentGen[i+1]==1 && ParentGen[i-1]==0) { // element 1
+						currentLine[i]=Rule[3];
+					}
+					else if(ParentGen[i]==0 && ParentGen[i+1]==0 && ParentGen[i-1]==1) { // element 1
+						currentLine[i]=Rule[4];
+					}
+					else if(ParentGen[i]==0 && ParentGen[i+1]==1 && ParentGen[i-1]==1) { // element 1
+						currentLine[i]=Rule[5];
+					}
+					else if(ParentGen[i]==1 && ParentGen[i+1]==0 && ParentGen[i-1]==1) { // element 1
+						currentLine[i]=Rule[6];
+					}
+					else if(ParentGen[i]==1 && ParentGen[i+1]==1 && ParentGen[i-1]==1) { // element 1
+						currentLine[i]=Rule[7];
+					}
+				}
+			}
+			printLine(currentLine);
+			for(int i=0; i<WIDTH; i++) ParentGen[i]=currentLine[i];
+		}
+	}
+
+	void CA1DLogic::setRule(int value) {
+		for(int i=0; i<8; i++) {
+			Rule[i] = value%2;
+			value/=2;
+		}
+	}
+
+int main() {
+	int rule, WIDTH, GENS;
+
+	cout<< "Width: ";
+	cin >> WIDTH;
+	cout<< "GENS: ";
+	cin >> GENS;
+	cout<<"Rule: ";
+	cin>>rule;
+	CA1DLogic cl(WIDTH, GENS);
+	cl.printRule(rule);
+	return 0;
+}
--- CA1DLogic.h
+++ CA1DLogic.h
@@ -5,17 +5,18 @@
 	Cellular Automaton
 *************************/
 
-#ifndef CA_1D_LOGIC_H_INCLUDED
-#define CA_1D_LOGIC_H_INCLUDED
+
+#include <iostream>
 
 using namespace std;
 
 class CA1DLogic {
 	public:
 		CA1DLogic(int width, int generations); //constructor for the class that gives the const's a value
-		void setRule(int value); //converts the decimal given into a binary array
-		void printRule(); //prints the output of the rule to the screen cell by cell or row by row
+		void printRule(int value); //prints the output of the rule to the screen cell by cell or row by row
 	private:
-		const int WIDTH, GENS; //to hold how wide the pattern can go and how many generations you have
-		int parentGen[WIDTH]; //the array holding the generation
-}
+		int WIDTH, GENS; //to hold how wide the pattern can go and how many generations you have
+		int Rule[8];
+		void setRule(int value); //converts the decimal given into a binary array
+		void printLine(int gen[]);
+};
